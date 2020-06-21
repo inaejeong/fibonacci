d2bb79ec4644605ad9f3c21e31cb4a725a9d58ef
Inae Jeong <inaejeong.fl@gmail.com>
2020-06-21 19:28:18 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 'use strict';
-function fib(n){
-    if(n === 0){
-      return 0;
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,1);
+// ここまででmapの初期値設定
 
-    } else if (n === 1){
-      return 1;
+function fib(n){
+    if (memo.has(n)){
+        return memo.get(n);
     }
-    return fib(n - 1)+ fib(n - 2);
+ const value = fib(n-1) + fib(n - 2);
+ memo.set(n, value);
+ return value;
+//  ここまでがフィボナッチ数列の関数。途中にmemo.setの指示を含めた。
 }
+
 const length = 40;
-for (let i = 0; i <= length;  i++){
+for (let i =0; i <= length; i++){
     console.log(fib(i));
 }
+// これが長さの定義。
